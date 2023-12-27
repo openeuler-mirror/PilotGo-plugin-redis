393423dd9c2503d440bd7ad82361259b50868696
wubijie <wubijie@kylinos.cn>
2023-12-27 15:58:40 +0800

    update vendor
    add extentions test and update vendor

--- server/go.mod
+++ server/go.mod
@@ -3,7 +3,7 @@ module openeuler.org/PilotGo/redis-plugin
 go 1.20
 
 require (
-	gitee.com/openeuler/PilotGo/sdk v0.0.0-20231220070046-4f5d0a39b48f
+	gitee.com/openeuler/PilotGo/sdk v0.0.0-20240110060359-307cb097716f
 	github.com/gin-gonic/gin v1.9.1
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/mysql v1.5.2
--- server/vendor/gitee.com/openeuler/PilotGo/sdk/plugin/client/handler.go
+++ server/vendor/gitee.com/openeuler/PilotGo/sdk/plugin/client/handler.go
@@ -33,7 +33,7 @@ func ReverseProxyHandler(c *gin.Context) {
 	proxy.ServeHTTP(c.Writer, c.Request)
 }
 
-func InfoHandler(c *gin.Context) {
+func infoHandler(c *gin.Context) {
 	v, ok := c.Get("__internal__client_instance")
 	if !ok {
 		response.Fail(c, gin.H{"status": false}, "未获取到client值信息")
@@ -53,7 +53,7 @@ func InfoHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, info)
 }
 
-func BindHandler(c *gin.Context) {
+func bindHandler(c *gin.Context) {
 	port := c.Query("port")
 
 	v, ok := c.Get("__internal__client_instance")
@@ -66,12 +66,18 @@ func BindHandler(c *gin.Context) {
 		response.Fail(c, gin.H{"status": false}, "client信息错误")
 		return
 	}
-	client.server = strings.Split(c.Request.RemoteAddr, ":")[0] + ":" + port
-
+	server := strings.Split(c.Request.RemoteAddr, ":")[0] + ":" + port
+	if client.server == "" {
+		client.server = server
+	} else if client.server != "" && client.server != server {
+		logger.Error("已有PilotGo-server与此插件绑定")
+	}
+	client.cond.Broadcast()
+	client.sendHeartBeat()
 	response.Success(c, nil, "bind server success")
 }
 
-func EventHandler(c *gin.Context) {
+func eventHandler(c *gin.Context) {
 	j, err := io.ReadAll(c.Request.Body) // 接收数据
 	if err != nil {
 		logger.Error("没获取到：%s", err.Error())
@@ -95,7 +101,7 @@ func EventHandler(c *gin.Context) {
 	client.ProcessEvent(&msg)
 }
 
-func ExtentionsHandler(c *gin.Context) {
+func extentionsHandler(c *gin.Context) {
 	v, ok := c.Get("__internal__client_instance")
 	if !ok {
 		response.Fail(c, gin.H{"status": false}, "未获取到client值信息")
@@ -110,7 +116,7 @@ func ExtentionsHandler(c *gin.Context) {
 	response.Success(c, client.extentions, "")
 }
 
-func CommandResultHandler(c *gin.Context) {
+func commandResultHandler(c *gin.Context) {
 	j, err := io.ReadAll(c.Request.Body) // 接收数据
 	if err != nil {
 		logger.Error("没获取到：%s", err.Error())
@@ -137,7 +143,7 @@ func CommandResultHandler(c *gin.Context) {
 
 }
 
-func TagsHandler(c *gin.Context) {
+func tagsHandler(c *gin.Context) {
 	j, err := io.ReadAll(c.Request.Body) // 接收数据
 	if err != nil {
 		logger.Error("没获取到：%s", err.Error())
