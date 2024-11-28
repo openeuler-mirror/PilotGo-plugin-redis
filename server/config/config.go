aa5f589ddcc87f5fabf720247cd16cf9606344c8
wubijie <wubijie@kylinos.cn>
2024-11-28 11:05:20 +0800

    add copyright information

--- server/config/config.go
+++ server/config/config.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) KylinSoft  Co., Ltd. 2024.All rights reserved.
+ * PilotGo-plugin-redis licensed under the Mulan Permissive Software License, Version 2. 
+ * See LICENSE file for more details.
+ * Author: wubijie <wubijie@kylinos.cn>
+ * Date: Mon Jun 26 15:24:19 2023 +0800
+ */
 package config
 
 import (
